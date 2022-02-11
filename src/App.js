1bcbc9490a0a18744eeed9f0fb83edfc238eb806
Geordi Dosher <geordi.dosher@gmail.com>
2022-02-11 08:46:07 -0600

    Tutorial complete

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (or don't)
         </a>
       </header>
     </div>
